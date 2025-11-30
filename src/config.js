6e8fa80ec68ef10be0c15dda8a3a668dd413d355
Arsalan-Fard <arsalan77x@gmail.com>
2025-11-30 16:41:56 +0100

    Stop tracking generated and env files

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export const CONFIG = {
-    accessToken: '',
-    style: 'mapbox://styles/mapbox/satellite-streets-v12',
-    center: [2.2457, 48.7145], // Palaiseau
-    zoom: 15,
-    pitch: 45,
-    bearing: -17.6
-};
