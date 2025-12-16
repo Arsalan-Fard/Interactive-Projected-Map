137567139f47691131d7051bb20ff8e0ba561706
Arsalan-Fard <arsalan77x@gmail.com>
2025-12-16 21:10:59 +0100

    Stop tracking static/js/config.js (it is now ignored)

--- static/js/config.js
+++
@@ -1,8 +0,0 @@
-export const CONFIG = {
-    accessToken: 'pk.eyJ1IjoiYXJzYWxhbjc3eCIsImEiOiJjbWk0cGMwOGEwNm9hMm1zZGE3em5jdG5oIn0.YqcXhom4SK5OtovP0wHgXA',
-    style: 'mapbox://styles/mapbox/light-v11',
-    center: [2.2, 48.714], // Palaiseau
-    zoom: 15,
-    pitch: 45,
-    bearing: 40
-};
